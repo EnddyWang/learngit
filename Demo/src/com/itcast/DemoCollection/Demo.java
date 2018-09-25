d68c37f1db231b7a8111d287680ef271ee9931a0
wangchong <wangchong@192.168.199.109>
2018-09-25 23:31:36 +0800

    本地git仓库提交

---
+++ Demo/src/com/itcast/DemoCollection/Demo.java
@@ -0,0 +1,37 @@
+package com.itcast.DemoCollection;
+
+import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Random;
+
+public class Demo {
+	private static final double advert = 0.40;
+	private static final double integral = 0.50;
+	private static final double redEnvelopes = 0.10;
+
+	public static void main(String[] args) {
+		Date date = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		System.out.println(60/10000);
+	}
+
+	private static int PercentageRandom() {
+		double randomNumber;
+		randomNumber = Math.random();
+		if (randomNumber >= 0 && randomNumber <= advert) {
+			// 返回广告数据
+			return 1;
+		} else if (randomNumber >= advert / 100 && randomNumber <= advert + integral) {
+			// 返回积分数据
+			return 2;
+		} else if (randomNumber >= advert + integral && randomNumber <= advert + integral + redEnvelopes) {
+			// 返回红包数据
+			return 3;
+		}
+		return -1;
+	}
+}
